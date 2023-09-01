e089b4671521286bb2226f887c783c0708dd0f09
GiovanniSmokes <138458840+GiovanniSmokes@users.noreply.github.com>
2023-09-01 05:31:48 +0100

    Update send.py

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ import resend  # make sure the 'resend' package is installed
 resend.api_key = "re_6PeVi179_HcrXp45jBArGYrAvVMJQGx6T"
 
 # Use the line below if you set the API key as an environment variable
-# resend.api_key = os.environ["re_6PeVi179_HcrXp45jBArGYrAvVMJQGx6T"] #
+# resend.api_key = os.environ["RESENDAPIKEY"] #
 
 params = {
     "from": 'cecconig@newaisolutions.com',
